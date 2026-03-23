bef05cca8f0210846e44068cc10d28f769cf468c
mohammadnoman365 <nomanmohammad365@gmail.com>
2026-03-23 05:03:58 +0500

    Update BulletLauncher.cs

--- Assets/Scripts/BulletLauncher.cs
+++ Assets/Scripts/BulletLauncher.cs
@@ -8,41 +8,32 @@ public class BulletLanucher : MonoBehaviour
     private ParticleSystem muzzleFlashParticleSystem;
     public Light muzzleFlashLight; 
 
-    // Start is called before the first frame update
     void Start()
     {
         muzzleFlash.SetActive(false);
 
-        // Get the ParticleSystem component from the muzzleFlash GameObject
         muzzleFlashParticleSystem = muzzleFlash.GetComponent<ParticleSystem>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            // Instantiate the bullet prefab at the position & rotation of the bullet launcher
             Instantiate(bullet, transform.position, transform.rotation);
 
-            // Play the bullet sound
             bulletSound.Play();
 
             muzzleFlash.SetActive(true);
 
-            // Restart the particle system
             muzzleFlashParticleSystem.Stop();
             muzzleFlashParticleSystem.Play();
 
-            // Enable the light
             muzzleFlashLight.enabled = true;
 
-            // Disable the muzzle flash & light after a delay
             Invoke(nameof(DisableMuzzleFlash), 0.1f);
         }
     }
 
-    // Function to disable the muzzle flash & light
     void DisableMuzzleFlash()
     {
         muzzleFlashParticleSystem.Stop(); 
